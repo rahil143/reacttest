1d70c604b4eda691e5f596fb06f28c96b8189a0b
rahil143 <pathanrahil143@gmail.com>
2020-05-30 20:02:37 +0530

    Adding DB Setup

---
+++ mern-exercise-tracker/backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// Connecting to the database
+const uri = process.env.ATLAS_URI;
+mongoose.Promise = global.Promise;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology : true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(port,()=>{
+    console.log(`Server is running on port: ${port}`)
+})
