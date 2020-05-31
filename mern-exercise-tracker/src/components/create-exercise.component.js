5f90849e9672036e22fbcb739f28e35189dfea39
rahil143 <pathanrahil143@gmail.com>
2020-05-31 12:48:47 +0530

    Added Navbar components

---
+++ mern-exercise-tracker/src/components/create-exercise.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class CreateExercise extends Component {
+
+    render() {
+      return (
+        <div>
+            <p>You are on Create Exercise Component</p>
+        </div>
+      );
+    }
+  }
