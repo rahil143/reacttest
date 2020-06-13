c7fcd3089e2a4f4fb0f3b01d1a738517eb7c4e44
rahil143 <pathanrahil143@gmail.com>
2020-06-13 14:51:43 +0530

    Test

--- mern-exercise-tracker/services/routes/auth.js
+++ mern-exercise-tracker/services/routes/auth.js
@@ -6,7 +6,7 @@ const Joi = require('@hapi/joi')
 
 const schema ={
     name : Joi.string().min(6).required(),
-    email : Joi.string().min(6).required(),
+    email : Joi.string().email().min(6).required(),
     password : Joi.string.min(6).require()
 }
 
